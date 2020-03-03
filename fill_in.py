e79e605fda7ed79f6c7c4abc7ae9c9e2add05940
Lassi Lehtinen <lokleh@utu.fi>
2020-03-03 11:09:26 +0200

    fill_in.py update

--- fill_in.py
+++ fill_in.py
@@ -1,17 +1,30 @@
 from tkinter import *
 
 class myFrame(Frame):
-    def __init__(self, location):
-        size = 9
-        self.frame = LabelFrame(location, text="Fill-in form", padx=10, pady=10)
-        for i in range(size):
-            Label(self.frame, text="Value " + str(i)).grid(row=i, column=0)
-            Entry(self.frame).grid(row=i, column=1)
+    def __init__(self, location, input_values, frame_name):
+        self.entrys = []
+        self.labels = []
+        self.frame = LabelFrame(location, text=frame_name, padx=10, pady=10)
+        for i in range(len(input_values)):
+            self.labels.append(Label(self.frame, text=input_values[i]))
+            self.labels[i].grid(row=i, column=0)
+            self.entrys.append(Entry(self.frame))
+            self.entrys[i].grid(row=i, column=1)
         self.frame.pack(padx=10, pady=10)
-        Button(self.frame, text="Submit").grid(row=size+1, column=0, columnspan=2, pady=10, padx=10)
+        Button(self.frame, text="Submit", command=self.get_entry_values).grid(row=i+1, column=0, columnspan=2, pady=10, padx=10)
+
+    def get_entry_values(self):
+        return_values = []
+        for entry in self.entrys:
+            return_values.append(entry.get())
+        #return return_values
+        print(return_values)
 
 root = Tk()
+values = ["Banaanit","Omenat","Sitruunat","Limet"]
+
 root.title("Useful App")
-myFrame(root)
+myFrame(root, values, "Hedelmät")
+myFrame(root, range(5), "Arvoja")
 
 root.mainloop()
